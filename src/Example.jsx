51f2dc3262499985c30d9f7eb8c55c30ad9fac40
0nahid <nahidhassanbulbul@gmail.com>
2023-08-25 00:09:42 +0600

    only the avaiable date can be in range or select date, others are blocked

--- src/Example.jsx
+++ src/Example.jsx
@@ -1,150 +1,3 @@
-// import { isSameDay } from "date-fns";
-// import { useEffect, useState } from "react";
-// import { DayPicker } from "react-day-picker";
-
-// export default function CustomCalendar() {
-//   const [selectedDays, setSelectedDays] = useState([]);
-//   const [ranges, setRanges] = useState([]); // Array of date ranges
-//   const [rangeStart, setRangeStart] = useState(null);
-//   const handleDayClick = (day, { selected }) => {
-//     if (rangeStart) {
-//       if (isSameDay(rangeStart, day)) {
-//         if (!selected) {
-//           setSelectedDays((prev) => [...prev, day]);
-//           console.log("Selected Individual Date:", day);
-//         }
-//         setRangeStart(null);
-//       } else {
-//         const sortedDates = [rangeStart, day].sort(
-//           (a, b) => a.getTime() - b.getTime()
-//         );
-//         const newRange = { from: sortedDates[0], to: sortedDates[1] };
-//         setRanges((prev) => [...prev, newRange]);
-//         console.log("Selected Range:", newRange);
-
-//         // Remove any individual dates that are now part of the range
-//         setSelectedDays((prev) =>
-//           prev.filter((d) => !isDateWithinRange(d, newRange))
-//         );
-
-//         setRangeStart(null);
-//       }
-//     } else {
-//       // Check if the selected date is part of any existing range
-//       const isPartOfRange = ranges.some((r) => isDateWithinRange(day, r));
-
-//       if (!isPartOfRange) {
-//         if (selected) {
-//           setSelectedDays((prev) => prev.filter((d) => !isSameDay(d, day)));
-//         } else {
-//           setSelectedDays((prev) => [...prev, day]);
-//           console.log("Selected Individual Date:", day);
-//         }
-//       }
-
-//       setRangeStart(day);
-//     }
-//   };
-
-//   const isDateWithinRange = (date, { from, to }) => {
-//     return date.getTime() >= from.getTime() && date.getTime() <= to.getTime();
-//   };
-
-//   const isDaySelected = (day) => {
-//     for (let range of ranges) {
-//       if (isDateWithinRange(day, range)) {
-//         return true;
-//       }
-//     }
-//     return selectedDays.some((selectedDay) => isSameDay(day, selectedDay));
-//   };
-
-//   const deselectIndividualDate = (date) => {
-//     setSelectedDays((prev) => prev.filter((d) => !isSameDay(d, date)));
-//   };
-
-//   const deselectRange = (index) => {
-//     setRanges((prev) => prev.filter((_, i) => i !== index));
-//   };
-
-//   // Dummy JSON data
-//   const jsonData = {
-//     unavailableDates: ["2023-08-26", "2023-08-27", "2023-08-28","2023-08-29"],
-//   };
-
-//   // Convert the string dates into actual date objects
-//   const [unavailableDates, setUnavailableDates] = useState([]);
-
-//   useEffect(() => {
-//     const convertedDates = jsonData.unavailableDates.map(
-//       (dateStr) => new Date(dateStr)
-//     );
-//     setUnavailableDates(convertedDates);
-//   }, []);
-
-//   let footer = (
-//     <p>Please click twice for individual dates or pick for a range.</p>
-//   );
-//   if (selectedDays.length > 0) {
-//     footer = <p>You selected {selectedDays.length} individual dates.</p>;
-//   }
-
-//   console.log(`unavailableDates:`, unavailableDates);
-
-//   return (
-//     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col md:flex-row w-full max-w-3xl">
-//       {/* Part 1: Calendar */}
-//       <div className="flex-none w-full md:w-1/2 mb-4 md:mb-0">
-//         <DayPicker
-//           onDayClick={handleDayClick}
-//           selected={isDaySelected}
-//           disabled={unavailableDates} // use the unavailableDates array here
-//           footer={footer}
-//           // hidden={unavailableDates}
-//           showOutsideDays
-//           className="border border-gray-300 rounded shadow p-5"
-//         />
-//       </div>
-
-//       {/* Part 2: Data Display */}
-//       <div className="flex-grow w-full md:w-1/2 bg-white p-8 overflow-y-auto max-h-[400px]">
-//         <h3 className="text-gray-700 border-b pb-2 mb-3">Selected Ranges:</h3>
-//         <ul className="list-decimal pl-5 mb-5">
-//           {ranges.map((range, index) => (
-//             <li
-//               key={index}
-//               onClick={() => deselectRange(index)}
-//               className="mb-2 p-2 bg-gray-200 rounded cursor-pointer hover:bg-gray-300 flex justify-between"
-//             >
-//               <span>
-//                 {range.from.toLocaleDateString()} -{" "}
-//                 {range.to.toLocaleDateString()}
-//               </span>
-//               <span className="text-red-500 ml-2">X</span>
-//             </li>
-//           ))}
-//         </ul>
-
-//         <h3 className="text-gray-700 border-b pb-2 mb-3 mt-5">
-//           Individual Dates:
-//         </h3>
-//         <ul className="list-decimal pl-5">
-//           {selectedDays.map((date, index) => (
-//             <li
-//               key={index}
-//               onClick={() => deselectIndividualDate(date)}
-//               className="mb-2 p-2 bg-gray-200 rounded cursor-pointer hover:bg-gray-300 flex justify-between"
-//             >
-//               <span>{date.toLocaleDateString()}</span>
-//               <span className="text-red-500 ml-2">X</span>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { isSameDay } from "date-fns";
 import { useEffect, useState } from "react";
 import { DayPicker } from "react-day-picker";
@@ -155,6 +8,8 @@ export default function CustomCalendar() {
   const [rangeStart, setRangeStart] = useState(null);
 
   const handleDayClick = (day, { selected }) => {
+    if (isDateUnavailable(day) || !isDateAvailable(day)) return; // If day is unavailable or not available, return
+
     if (rangeStart) {
       if (isSameDay(rangeStart, day) || isDateDisabled(day)) {
         if (!selected) {
@@ -211,27 +66,70 @@ export default function CustomCalendar() {
   };
 
   const jsonData = {
-    unavailableDates: ["2023-08-26", "2023-08-27", "2023-08-28", "2023-08-29"],
+    availableDates: [
+      "2023-08-15",
+      "2023-08-16",
+      "2023-08-17",
+      "2023-08-18",
+      "2023-08-20",
+      "2023-08-21",
+      "2023-08-22",
+      "2023-08-24",
+      "2023-08-25",
+      "2023-08-30",
+      "2023-09-01",
+      "2023-09-02",
+      "2023-09-03",
+      "2023-09-04",
+      "2023-09-05",
+      "2023-09-06",
+      "2023-09-07",
+    ],
+    unavailableDates: ["2023-08-26", "2023-08-27", "2023-08-28"],
   };
 
+  const [availableDates, setAvailableDates] = useState([]);
   const [unavailableDates, setUnavailableDates] = useState([]);
 
   useEffect(() => {
-    const convertedDates = jsonData.unavailableDates.map(
+    const convertedAvailableDates = jsonData.availableDates.map(
       (dateStr) => new Date(dateStr)
     );
-    setUnavailableDates(convertedDates);
+    const convertedUnavailableDates = jsonData.unavailableDates.map(
+      (dateStr) => new Date(dateStr)
+    );
+    setAvailableDates(convertedAvailableDates);
+    setUnavailableDates(convertedUnavailableDates);
   }, []);
-
-  const isRangeIncludingDisabledDates = ({ from, to }) => {
-    return unavailableDates.some(
-      (disabledDate) =>
-        disabledDate.getTime() >= from.getTime() &&
-        disabledDate.getTime() <= to.getTime()
+  const isDateAvailable = (date) => {
+    return availableDates.some((availableDate) =>
+      isSameDay(availableDate, date)
     );
   };
 
+  const isDateUnavailable = (date) => {
+    return unavailableDates.some((unavailableDate) =>
+      isSameDay(unavailableDate, date)
+    );
+  };
+
+  const isRangeIncludingDisabledDates = ({ from, to }) => {
+    let date = new Date(from);
+    while (date <= to) {
+      if (!isDateAvailable(date) || isDateUnavailable(date)) {
+        return true;
+      }
+      date.setDate(date.getDate() + 1);
+    }
+    return false;
+  };
+
   const isDateDisabled = (date) => {
+    return unavailableDates.some((disabledDate) =>
+      isSameDay(disabledDate, date)
+    );
+  };
+  const isDateExplicitlyUnavailable = (date) => {
     return unavailableDates.some((disabledDate) =>
       isSameDay(disabledDate, date)
     );
@@ -254,17 +152,23 @@ export default function CustomCalendar() {
         <DayPicker
           onDayClick={handleDayClick}
           selected={isDaySelected}
-          disabled={unavailableDates}
+          disabled={(day) =>
+            isDateExplicitlyUnavailable(day) || !isDateAvailable(day)
+          }
           footer={footer}
           showOutsideDays
           className="border border-gray-300 rounded shadow p-5"
           modifiers={{
-            disabled: unavailableDates,
+            available: availableDates,
+            unavailable: unavailableDates,
             selected: selectedDays,
             ranges: ranges,
           }}
           modifiersStyles={{
-            disabled: {
+            available: {
+              color: "green",
+            },
+            unavailable: {
               color: "red",
             },
             selected: {
